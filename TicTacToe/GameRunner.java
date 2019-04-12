acd204b2776200d0597dfeb84743662fa19de756
COMPUTER\vasek <vasika@gmail.com>
2019-04-12 09:42:32 +0300

    v2

--- TicTacToe/GameRunner.java
+++
@@ -1,9 +0,0 @@
-package TicTacToe;
-
-// главный класс приложения, содержащий метод main
-public class GameRunner {
-    public static void main(String[] args) {
-        GameLogic gameLogic = new GameLogic();
-        gameLogic.startGame();
-    }
-}
