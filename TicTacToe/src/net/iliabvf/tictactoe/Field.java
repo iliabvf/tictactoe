b8f0faefc5fc62861ce4dff32f5d628bde94fe64
COMPUTER\vasek <vasika@gmail.com>
2019-04-12 09:46:22 +0300

    v2.1

---
+++ TicTacToe/src/net/iliabvf/tictactoe/Field.java
@@ -0,0 +1,33 @@
+package net.iliabvf.tictactoe;
+
+// сущность, описывающая игровое поле
+class Field {
+    private char[][] fieldData;
+
+    Field() {
+        char[][] fieldData = new char[3][3];
+        for (int r = 0; r < 3; r++){
+            for (int c = 0; c < 3; c++){
+                fieldData[r][c] = ' ';
+            }
+        }
+
+        this.fieldData = fieldData;
+    }
+
+    public char[][] getFieldData() {
+        return fieldData;
+    }
+
+    public void setFieldData(char[][] fieldData) {
+        this.fieldData = fieldData;
+    }
+
+    public void setFieldCell(int row, int col, char value) {
+        this.fieldData[row][col] = value;
+    }
+
+    public char getFieldCell(int row, int col) {
+        return this.fieldData[row][col];
+    }
+}
---
+++ TicTacToe/src/net/iliabvf/tictactoe/GameRunner.java
@@ -0,0 +1,9 @@
+package net.iliabvf.tictactoe;
+
+public class GameRunner {
+
+    public static void main(String[] args) {
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.startGame();
+    }
+}
